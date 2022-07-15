a048f9d6f2727c13de7daf07151de6966cdd33fd
Nicholas Denaro <ndenarodev@gmail.com>
2022-07-15 17:14:29 -0400

    Some dice that can roll

---
+++ Game/Game/Dice.cs
@@ -0,0 +1,137 @@
+﻿using GameEngine;
+using GameEngine._2D;
+using GameEngine.Interfaces;
+using GameEngine.UI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Game.Program;
+
+namespace Game
+{
+    internal class Dice : Entity
+    {
+        private Description2D description;
+        private Description2D descriptionFace;
+        private Entity diceFace;
+        private bool rolling;
+        private int rollingTime;
+
+        private int[] faces;
+        private int index;
+        private bool held;
+
+        public static Dice Create(GameEngine.GameEngine engine, int state, (Sides sides, Colors color, Faces[] faces) info, int x, int y)
+        {
+            return Create(engine, state, info.sides, info.color, info.faces, x, y);
+        }
+
+        private static Dice Create(GameEngine.GameEngine engine, int state, Sides sides, Colors color, Faces[] faces, int x, int y)
+        {
+            var dice = new Dice((int)sides, (int)color, faces.Select(f => (int)f).ToArray(), x, y);
+            engine.AddEntity(state, dice);
+            engine.AddEntity(state, dice.diceFace);
+
+            return dice;
+        }
+
+        private Dice(int sides, int color, int[] faces, int x, int y) : base(new Description2D(Sprite.Sprites["dice"], x, y, 32, 32))
+        {
+            this.description = this.Description as Description2D;
+            this.diceFace = new Entity(this.descriptionFace = new Description2D(Sprite.Sprites["diceFaces"], x, y, 10, 10));
+            this.faces = faces;
+            this.descriptionFace.ImageIndex = this.faces[index];
+            this.description.ImageIndex = sidesToIndex(sides) + color * 6;
+        }
+
+        private static int sidesToIndex(int sides)
+        {
+            switch(sides)
+            {
+                case 4:
+                    return 0;
+                case 6:
+                    return 1;
+                case 8:
+                    return 2;
+                case 10:
+                    return 3;
+                case 12:
+                    return 4;
+                case 20:
+                    return 5;
+                default:
+                    return 0;
+            }
+        }
+
+        public void Roll()
+        {
+            this.rolling = true;
+            this.rollingTime = Program.Random.Next(2 * Program.FPS, 3 * Program.FPS);
+        }
+
+        private Point lastMousePos;
+        public override void Tick(GameState state)
+        {
+            if (!rolling && state.Controllers[0][Keys.RCLICK].IsPress())
+            {
+                MouseControllerInfo info = state.Controllers[0][Keys.RCLICK].Info as MouseControllerInfo;
+                if (description.IsCollision(new Description2D(info.X + this.description.Sprite.X, info.Y + this.description.Sprite.Y, 1, 1)))
+                {
+                    this.held = true;
+                    lastMousePos = new Point(info.X, info.Y);
+                }
+            }
+
+            if (this.held && state.Controllers[0][Keys.RCLICK].IsDown())
+            {
+                MouseControllerInfo info = state.Controllers[0][Keys.RCLICK].Info as MouseControllerInfo;
+
+                this.description.ChangeCoordsDelta(info.X - lastMousePos.X, info.Y - lastMousePos.Y);
+                this.descriptionFace.ChangeCoordsDelta(info.X - lastMousePos.X, info.Y - lastMousePos.Y);
+
+                lastMousePos = new Point(info.X, info.Y);
+            }
+
+            if (this.held && !state.Controllers[0][Keys.RCLICK].IsDown())
+            {
+                MouseControllerInfo info = state.Controllers[0][Keys.RCLICK].Info as MouseControllerInfo;
+                if (description.IsCollision(new Description2D(info.X + this.description.Sprite.X, info.Y + this.description.Sprite.Y, 1, 1)))
+                {
+                    this.held = false;
+                }
+            }
+
+            if (this.rolling && this.rollingTime-- > 0)
+            {
+                if (this.rollingTime > 2 * Program.FPS && this.rollingTime % 2 == 0)
+                {
+                    descriptionFace.ImageIndex = this.faces[index++ % this.faces.Length];
+                }
+                else if (this.rollingTime > 1 * Program.FPS && this.rollingTime % 3 == 0)
+                {
+                    descriptionFace.ImageIndex = this.faces[index++ % this.faces.Length];
+                }
+                else if (this.rollingTime > 1.5 * Program.FPS && this.rollingTime % 6 == 0)
+                {
+                    descriptionFace.ImageIndex = this.faces[index++ % this.faces.Length];
+                }
+                else if (this.rollingTime > 1 * Program.FPS && this.rollingTime % 8 == 0)
+                {
+                    descriptionFace.ImageIndex = this.faces[index++ % this.faces.Length];
+                }
+                else if (this.rollingTime % 10 == 0)
+                {
+                    descriptionFace.ImageIndex = this.faces[index++ % this.faces.Length];
+                }
+            }
+            else
+            {
+                this.rolling = false;
+            }
+        }
+    }
+}
---
+++ Game/Game/Program.cs
@@ -0,0 +1,90 @@
+﻿using GameEngine;
+using GameEngine._2D;
+using GameEngine.UI;
+using GameEngine.UI.AvaloniaUI;
+using GameEngine.UI.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    internal class Program
+    {
+        public const int FPS = 60;
+
+        public static readonly Random Random = new Random();
+
+        static async Task Main()
+        {
+            (GameEngine.GameEngine engine, GameUI ui) = new GameBuilder()
+                .GameEngine(new FixedTickEngine(FPS))
+                .GameView(new GameView2D(new Drawer2DAvalonia(), 480, 320, 2, 2, Color.Gray))
+                .GameFrame(new GameUI(
+                    new AvaloniaWindowBuilder()
+                        .StartupLocation(Avalonia.Controls.WindowStartupLocation.CenterScreen)
+                        .Title("Game")
+                    , 0, 0, 480, 320, 2, 2
+                    ))
+                .Controller(new WindowsMouseController(mouseMap))
+                .Controller(new WindowsKeyController(keyMap))
+                .StartingLocation(new Location(new Description2D(0, 0, 480, 320)))
+                .Build();
+
+            
+            Sprite diceSprite = new Sprite("dice", "Sprites.dice.png", 32, 32, 16, 16);
+            Sprite diceFacesSprite = new Sprite("diceFaces", "Sprites.diceFaces.png", 10, 10, 5, 5);
+            Sprite deskSprite = new Sprite("desk", "Sprites.desk.png", 480, 320, 0, 0);
+            engine.AddEntity(0, new Entity(new Description2D(deskSprite, 0, 0)));
+
+            Dice[] dice = new Dice[10];
+            for (int i = 0; i < dice.Length; i++)
+            {
+                var keys = new List<string>(DicePresets.Keys);
+                dice[i] = Dice.Create(engine, 0, DicePresets[keys[Random.Next(keys.Count)]], 60 + i * 40, 60);
+            }
+
+            engine.TickEnd(0) += (object s, GameState state) =>
+            {
+                if (state.Controllers[1][Keys.ROLL].IsPress())
+                {
+                    for (int i = 0; i < dice.Length; i++)
+                    {
+                        dice[i].Roll();
+                    }
+                }
+            };
+
+            await engine.Start();
+        }
+
+        public static Dictionary<int, object> mouseMap = new Dictionary<int, object>()
+        {
+            { (int)Avalonia.Input.PointerUpdateKind.LeftButtonPressed, Keys.CLICK },
+            { (int)Avalonia.Input.PointerUpdateKind.MiddleButtonPressed, Keys.RCLICK },
+        };
+
+        public static Dictionary<int, object> keyMap = new Dictionary<int, object>()
+        {
+            { (int)Avalonia.Input.Key.R, Keys.ROLL },
+        };
+
+        public enum Sides { FOUR = 4, SIX = 6, EIGHT = 8, TEN = 10, TWELVE = 12, TWENTY = 20 }
+
+        public enum Colors { RED = 0, BLUE = 1, GREEN = 2, YELLOW = 3 }
+
+        public enum Faces { NONE = 0, SWORD = 1, SHIELD = 2, HEAL = 3, BOW = 4}
+
+        public enum Keys { CLICK, RCLICK, ROLL }
+
+        public static Dictionary<string, (Sides sides, Colors color, Faces[] faces)> DicePresets = new Dictionary<string, (Sides sides, Colors color, Faces[] faces)>()
+        {
+            { "Red Warrior", (Sides.FOUR, Colors.RED, new []{ Faces.SWORD, Faces.SHIELD, Faces.SWORD, Faces.SHIELD }) },
+            { "Red Archer", (Sides.FOUR, Colors.RED, new []{ Faces.BOW, Faces.SHIELD, Faces.BOW, Faces.SHIELD }) },
+            { "Red Healer", (Sides.FOUR, Colors.RED, new []{ Faces.HEAL, Faces.SHIELD, Faces.SWORD, Faces.SHIELD }) },
+            { "Blue Warrior", (Sides.FOUR, Colors.BLUE, new []{ Faces.SWORD, Faces.HEAL, Faces.SWORD, Faces.SHIELD }) },
+            { "Blue Knight", (Sides.EIGHT, Colors.BLUE, new []{ Faces.SWORD, Faces.SHIELD, Faces.HEAL, Faces.SHIELD, Faces.SWORD, Faces.SHIELD, Faces.SWORD, Faces.SHIELD }) },
+        };
+    }
+}
